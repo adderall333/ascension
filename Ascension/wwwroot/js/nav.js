dba812c0e8551e273a2e08917153be5fa3a5fd4b
Kirill <kirillaromad@mail.ru>
2021-03-06 18:44:43 +0300

    navbar done

--- Ascension/wwwroot/js/nav.js
+++ Ascension/wwwroot/js/nav.js
@@ -1,20 +1,40 @@
-﻿// ---------Responsive-navbar-active-animation-----------
-function test(){
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px",
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
+﻿function test(){
+    
+    var location = window.location.href; 
+    var cur_url = '/' + location.split('/').pop(); // текущий адрес
+
+    //var cur_active;
+    $('.navbar-collapse li').each(function () { // проходимся по всем пунктам меню
+        var link = $(this).find('a').attr('href'); // выделяем ссылку 
+        if(cur_url === link){
+            //cur_active = $(this);
+            $(this).addClass('active'); // если ссылка является текущим url, то <li> мы добавляем класс .active
+        }
     });
+    
+    // определяем координаты активной ссылки
+    var tabs = $('#navbarSupportedContent');
+    var activeItem = tabs.find('.active');
+    var activeHeight = activeItem.innerHeight();
+    var activeWidth = activeItem.innerWidth();
+    var itemPosTop = activeItem.position();
+    var itemPosLeft = activeItem.position();
+    
+    // изменяем расположение элемента выделения активной ссылки
+    $(".hori-selector").css({
+        "top":itemPosTop.top + "px",
+        "left":itemPosLeft.left + "px",
+        "height": activeHeight + "px",
+        "width": activeWidth + "px"
+    });
+
+    $(".hori-selector").show(); // отображаем элемент выделения
+    
+    /*
     $("#navbarSupportedContent").on("click","li",function(e){
-        $('#navbarSupportedContent ul li').removeClass("active");
+        //$('#navbarSupportedContent ul li').removeClass("active");
+        cur_active.removeClass("active");
+        
         $(this).addClass('active');
         var activeWidthNewAnimHeight = $(this).innerHeight();
         var activeWidthNewAnimWidth = $(this).innerWidth();
@@ -26,13 +46,16 @@ function test(){
             "height": activeWidthNewAnimHeight + "px",
             "width": activeWidthNewAnimWidth + "px"
         });
+        
     });
+    */
 }
+
 $(document).ready(function(){
     setTimeout(function(){ test(); });
 });
 $(window).on('resize', function(){
-    setTimeout(function(){ test(); }, 500);
+    setTimeout(function(){ test(); });
 });
 $(".navbar-toggler").click(function(){
     setTimeout(function(){ test(); });
