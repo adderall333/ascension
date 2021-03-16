ad5ada38700bbf9d69591fadca2768a5a377167e
Danila Makarov <makarov01V@ya.ru>
2021-03-16 20:43:53 +0300

    changes&testing

---
+++ Ascension/Controllers/IPController.fs
@@ -0,0 +1,14 @@
+﻿namespace Ascension
+
+open Dadata
+open Dadata.Model
+open Microsoft.AspNetCore.Mvc
+open Newtonsoft.Json
+
+type IPController() =
+    inherit Controller()
+
+    let api = new SuggestClientAsync("ef2db2da426469acd403d525ff8241bcb5487ef6")
+    let response =  api.Geolocate(11231.1, 11111.1)
+    let address = response.suggestion[0].data
+    
--- Models/IP.cs
+++ Models/IP.cs
@@ -1,7 +1,17 @@
-﻿namespace Models
+﻿using System.Threading.Tasks;
+using Dadata;
+
+namespace Models
 {
     public class IP
     {
-        
+        public async static Task<string> City()
+        {
+            var api = new SuggestClientAsync("ef2db2da426469acd403d525ff8241bcb5487ef6");
+            var response = await api.Geolocate(lat: 55.878, lon: 37.653);
+            var address = response.suggestions[0].data.city;
+
+            return address;
+        }
     }
 }
