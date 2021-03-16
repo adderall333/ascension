ac4d36e9279265729ff1c6228037092430ee451d
Danila Makarov <makarov01V@ya.ru>
2021-03-16 20:57:39 +0300

    ipcontroller

--- Ascension/Controllers/IPController.fs
+++ Ascension/Controllers/IPController.fs
@@ -1,5 +1,6 @@
 ﻿namespace Ascension
 
+open System.ComponentModel
 open Dadata
 open Dadata.Model
 open Microsoft.AspNetCore.Mvc
@@ -8,7 +9,8 @@ open Newtonsoft.Json
 type IPController() =
     inherit Controller()
 
+    
     let api = new SuggestClientAsync("ef2db2da426469acd403d525ff8241bcb5487ef6")
-    let response =  api.Geolocate(11231.1, 11111.1)
-    let address = response.suggestion[0].data
+    let response = api.Geolocate(11231.1, 11111.1) |> Async.AwaitTask |> Async.RunSynchronously
+    let address = response.suggestions.[0].data.city 
     
