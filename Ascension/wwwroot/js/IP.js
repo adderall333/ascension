9625ec8803cc1ce38ba060e3531421c8320ad11c
Danila Makarov <makarov01V@ya.ru>
2021-03-19 07:01:18 +0300

    changes

--- Ascension/wwwroot/js/IP.js
+++ Ascension/wwwroot/js/IP.js
@@ -17,3 +17,14 @@
             .then(x => x1.innerHTML = x.city)
     }
 }
+
+$(function() {
+    $('.pop-up').hide();
+    $('.pop-up').fadeIn(1000);
+    $('.close-button').click(function (e) {
+        $('.pop-up').fadeOut(700);
+        $('#overlay').removeClass('blur-in');
+        $('#overlay').addClass('blur-out');
+        e.stopPropagation();
+    });
+});
