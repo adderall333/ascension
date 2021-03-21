eaa2ba798978db8e57c2bd7094922decbe7b5e25
Danila Makarov <makarov01V@ya.ru>
2021-03-22 02:07:24 +0300

    changes

--- Ascension/wwwroot/js/IP.js
+++ Ascension/wwwroot/js/IP.js
@@ -1,7 +1,22 @@
-﻿window.onload = function() {
-    var x1 = document.getElementById("demo");
+﻿const CITY_KEY = 'x-city';
+const setCity = city => localStorage.setItem(CITY_KEY, city);
+const getCity = () => localStorage.getItem(CITY_KEY);
+
+const updateCityElem = (x) => {
+    const city = getCity();
+    if (city) {
+        x.innerHTML = city;
+    }
+}
+
+window.onload = function() {
+    checkCity();
+    const x1 = document.getElementById("demo");
 
     document.getElementById("get-btn").addEventListener('click', getLocation);
+    
+    
+    updateCityElem(x1);
     
     function getLocation() {
         if (navigator.geolocation) {
@@ -14,14 +29,30 @@
     function showPosition(position) {
         fetch('/geoapi/get?lat=' + position.coords.latitude +'&lot='+ position.coords.longitude)
             .then(x => x.json())
-            .then(x => x1.innerHTML = x.city)
+            .then(({ city }) => {
+                setCity(city)
+                updateCityElem(x1)
+            })
             
     }
 }
 
+function checkCity() {
+    const city = getCity();
+    if (city) {
+        $('.pop-up').hide();
+    }
+     const cityEl = document.querySelector('#demo');
+     updateCityElem(cityEl);
+}
+
 $(function() {
     $('.pop-up').hide();
-    $('.pop-up').fadeIn(1000);
+    const city = getCity();
+    if (city) {
+        return;
+    }
+    $('.pop-up').fadeIn(1200);
     $('.close-button').click(function (e) {
         $('.pop-up').fadeOut(700);
         $('#overlay').removeClass('blur-in');
