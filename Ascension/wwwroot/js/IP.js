9fedfe64d20a1925d9426efca6e05dbe3b40c248
Danila Makarov <makarov01V@ya.ru>
2021-03-23 19:00:31 +0300

    changes

--- Ascension/wwwroot/js/IP.js
+++ Ascension/wwwroot/js/IP.js
@@ -102,6 +102,7 @@ $(function() {
         $('#overlay').removeClass('blur-in');
         $('#overlay').addClass('blur-out');
         e.stopPropagation();
+        location.reload()
     });
 });
 
