6b099490a07a6dafeaf83310f5c6adb58aac402c
Kirill <kirillaromad@mail.ru>
2021-03-24 19:18:31 +0300

    added error input style and some fix

--- Ascension/wwwroot/js/signup.js
+++ Ascension/wwwroot/js/signup.js
@@ -8,6 +8,11 @@ signup_button.addEventListener("click", () => {
     let pass = form.pass.value;
     let re_pass = form.re_pass.value;
 
+    for(let i=0; i<form.elements.length; i++) {
+        let input = form.elements[i];
+        input.classList.remove('error');
+    }
+
     let checkName = /^[A-Z][a-zA-Z]+$/.test(name);
     let checkSurname = /^[A-Z][a-zA-Z]+$/.test(surname);
     let checkEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
@@ -30,39 +35,60 @@ signup_button.addEventListener("click", () => {
         error_block.appendChild(div);
     }
     
+    function checkInputs() {
+        let check = true;
+        
+        for(let i=0; i<form.elements.length; i++) {
+            let input = form.elements[i];
+            if (input.type !== 'submit' && input.value.length < 1) {
+                check = false;
+                input.classList.add('error');
+            }
+        }
+        
+        if (!check) {
+            createErrorMessage('zmdi-close','Fill in all the fields');
+            return false;
+        }
+        
+        if (!checkName) {
+            check = false;
+            createErrorMessage('zmdi-close','Invalid name');
+            form.name.classList.add('error');
+        }
+        if (!checkSurname) {
+            check = false;
+            createErrorMessage('zmdi-close','Invalid surname');
+            form.surname.classList.add('error');
+        }
+        if (!checkEmail) {
+            check = false;
+            createErrorMessage('zmdi-close','Invalid email');
+            form.email.classList.add('error');
+        }
+        if (!checkPass) {
+            check = false;
+            createErrorMessage('zmdi-close','Invalid password' +
+                '</br>The password must be at least 6 characters long and contain at least one number, one uppercase and one lowercase letter');
+            form.pass.classList.add('error');
+        }
+        if (!checkRePass) {
+            check = false;
+            createErrorMessage('zmdi-close','Password mismatch');
+            form.re_pass.classList.add('error');
+        }
+        
+        return check;
+    }
+    
     let error_block = document.getElementById('error-block');
     while (error_block.firstChild) {
         error_block.removeChild(error_block.lastChild);
     }
 
-    if (name.length < 1 || surname.length < 1 || email.length < 1 || pass.length < 1 || re_pass.length < 1) {
-        createErrorMessage('zmdi-close','Fill in all the fields');
+    let check = checkInputs();
+    if (!check)
         return;
-    }
-    else {
-        if (!checkName) {
-            createErrorMessage('zmdi-close','Invalid name')
-            return;
-        }
-        if (!checkSurname) {
-            createErrorMessage('zmdi-close','Invalid surname')
-            return;
-        }
-        if (!checkEmail) {
-            createErrorMessage('zmdi-close','Invalid email')
-            return;
-        }
-        if (!checkPass) {
-            createErrorMessage('zmdi-close','Invalid password.\nThe password must be at least 6 characters long and contain at least one number, one uppercase and one lowercase letter.')
-            return;
-        }
-        if (!checkRePass) {
-            createErrorMessage('zmdi-close','Password mismatch')
-            return;
-        }
-    }
-    
-    // отправлять запрос на сервер
 
     let fD = new FormData();
     fD.append('name', name);
@@ -80,13 +106,14 @@ signup_button.addEventListener("click", () => {
             let result = xhr.getResponseHeader("registration_result")
             if (result === "ok") {
                 createErrorMessage('zmdi-check','You have successfully registered in our site');
-                //setTimeout(() => location.replace('/Authentication/Signin'), 3000)
                 Timer();
             }
-            else if (result === "failed")
-                createErrorMessage('zmdi-close','This email address already exists.\nPlease choose a unique one.')
+            else if (result === "failed") {
+                createErrorMessage('zmdi-close','This email address already exists</br>Please choose a unique one');
+                form.email.classList.add('error');
+            }
             else // "error"
-                createErrorMessage('zmdi-close','An error occurred while registering.\nPlease try again')
+                createErrorMessage('zmdi-close','An error occurred while registering</br>Please try again')
         }
     })
     
