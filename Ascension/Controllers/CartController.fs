a29bb4066bb691786e6f0132eb5cb21d25c709c3
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-04-26 17:55:47 +0300

    add structure of cart logic

--- Ascension/Controllers/CartController.fs
+++ Ascension/Controllers/CartController.fs
@@ -19,11 +19,25 @@ type CartController () =
         this.View(productLines)
         
     [<HttpPost>]  
-    member this.AddProduct(productId : int, userId : int) =
+    member this.AddProductLine(productId : int, userId : int) =
         use context = new ApplicationContext()
         context.ProductLine.Add(ProductLine (userId, productId)) |> ignore
         context.SaveChanges |> ignore
         
+    [<HttpPost>]
+    member this.RemoveProductLine(productId : int, userId : int) =
+        None
+    
+    [<HttpPost>]
+    member this.AddProduct(productId : int, userId : int) =
+        None
+        //проверку на существование продуктлайна не забыть, если его нету ничего не делать
+        
+    [<HttpPost>]
+    member this.RemoveProduct(productId : int, userId : int) =
+        None
+        //и тут проверку
+            
         
         
         
