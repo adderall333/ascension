5f7f4b997c9bab95cd95f54a8f425aa5bbc051c3
Dqnilq <makarov01V@ya.ru>
2021-04-26 17:16:04 +0300

    changes

---
+++ Ascension/Controllers/CartController.fs
@@ -0,0 +1,31 @@
+﻿namespace Ascension.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open System.Diagnostics
+
+open Microsoft.AspNetCore.Mvc
+open Models
+
+type CartController () =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Cart () =
+        this.View()
+        
+    [<HttpPost>]  
+    member this.AddProduct(id:int, user:int) =
+        use context = new ApplicationContext()
+        context.ProductLine.Add(ProductLine (user, id)) |> ignore
+        context.SaveChanges |> ignore
+        
+        
+        
+        
+   
+    
+    
+        
---
+++ Models/ProductLine.cs
@@ -0,0 +1,21 @@
+﻿namespace Models
+{
+    public class ProductLine
+    {
+        public int Id { get; set; }
+        
+        public  int ClientId { get; set; }
+
+        public int ProductId { get; set; }
+        
+        public int ProductCount { get; set; }
+
+
+        public ProductLine(int clientId, int productId)
+        {
+            ClientId = clientId;
+            ProductId = productId;
+            ProductCount = 1;
+        }
+    }
+}
