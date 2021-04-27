8d3ff897174a157480f139fbf715ad6d76e8d815
Kamil Nazmiev <kamilotina2001@gmail.com>
2021-04-27 17:07:25 +0300

    Added html, css, controller to profile

---
+++ Ascension/Controllers/ProfileController.fs
@@ -0,0 +1,21 @@
+﻿namespace Ascension
+
+open System
+open System.Collections.Generic
+open ProductFilter
+open System.Diagnostics
+open System.Linq
+open Microsoft.AspNetCore.Mvc
+open Microsoft.EntityFrameworkCore
+open Models
+
+type ProfileController() =
+    inherit Controller()
+
+
+    member this.Personal () =
+        this.View()
+        
+    member this.Order () =
+        this.View()
+
