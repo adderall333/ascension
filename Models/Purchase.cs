7019f4a31a323873072fdcf16e0f7150c30dbe4b
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-05-18 16:13:25 +0300

    update statistics

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -1,4 +1,6 @@
+using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace Models
 {
@@ -13,5 +15,34 @@ namespace Models
         
         public Product SecondProduct { get; set; }
         public int SecondProductId { get; set; }
+
+        public static void UpdatePurchases(Order order, ApplicationContext context)
+        {
+            var productLines = order.ProductLines;
+            var combinations = productLines
+                .SelectMany(productLine => productLines, Tuple.Create)
+                .Where(tuple => tuple.Item1.Id != tuple.Item2.Id);
+
+            foreach (var (productLine1, productLine2) in combinations)
+            {
+                var purchase = context
+                    .Purchase
+                    .FirstOrDefault(p => p.FirstProductId == productLine1.Product.Id &&
+                                                 p.SecondProductId == productLine2.Product.Id);
+
+                if (purchase == null)
+                {
+                    purchase = new Purchase
+                    {
+                        FirstProduct = productLine1.Product, 
+                        SecondProduct = productLine2.Product
+                    };
+                    context.Purchase.Add(purchase);
+                }
+
+                purchase.Count++;
+                context.SaveChanges();
+            }
+        }
     }
 }
